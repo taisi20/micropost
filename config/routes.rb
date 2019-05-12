436515ab13a172e4cfc4219d13b53bba1697c722
taisi20 <tokudotai@outlook.jp>
2019-05-12 19:25:13 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "toppages#index"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :new, :create]
 end
---
+++ db/migrate/20190511135632_rename_password_dijest_column_to_users.rb
@@ -0,0 +1,5 @@
+class RenamePasswordDijestColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :users, :password_dijest, :password_digest
+  end
+end
