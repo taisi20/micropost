4fe53cef103a6eda60d6aec6aa6372adf68f2b28
taisi20 <tokudotai@outlook.jp>
2019-06-13 13:43:52 +0000

    AWS_ACCESS_KEY_IDを隠した

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AKIAXPWP6YKSR3YAV3MW'],
-      aws_secret_access_key: ENV['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'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'us-east-1' # S3バケット作成時に指定したリージョン。左記は東京を指す
     }
     config.fog_directory  = 'microposts' # 作成したS3バケット名
