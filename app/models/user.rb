5d174742742853a5f505926830898ecfeca2bcad
taisi20 <tokudotai@outlook.jp>
2019-06-14 13:18:17 +0000

    user モデルに管理者識別のためのadminカラムを追加、users/indexで管理者としてログインした場合は他ユーザーのアカウントを削除できる仕様に変更　

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # picture_uploaderの据え付け
   mount_uploader :picture, PictureUploader
-
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
